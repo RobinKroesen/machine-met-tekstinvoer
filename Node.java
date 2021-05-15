b3df2125418870826f9668216665cb7ecae96ffa
RobinKroesen <74556141+RobinKroesen@users.noreply.github.com>
2021-05-15 17:28:59 +0200

    Upload van Node class
    De node class voor de machine

---
+++ Node.java
@@ -0,0 +1,19 @@
+package Nodes;
+
+public class Node {
+
+    java.lang.String direction1;
+    java.lang.String direction2;
+    java.lang.String nodeName;
+
+    public Node(java.lang.String direction1, java.lang.String direction2, java.lang.String nodeName) {
+        this.direction1 = direction1;
+        this.direction2 = direction2;
+        this.nodeName = nodeName;
+    }
+
+
+}
+
+
+
